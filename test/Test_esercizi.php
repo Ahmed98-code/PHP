d9a5521c1dcd17eef06c76345452e5f960b0edcd
Master <ahmed.darrag@engimtorino.net>
2021-08-03 23:20:57 +0200

    "error fix"

--- test/Test_esercizi.php
+++ test/Test_esercizi.php
@@ -9,7 +9,9 @@
     $ar[$len-$i-1] = $temp;
   }
 
-  print_r($ar)
+  print_r($ar);
+
+//---------------------------------------------------------------------------------------------------------------------
 
 //numeri primi
 /*Ad ogni modo per numeri primi si intendono tutti quei numeri interi positivi che hanno solo due divisori. In altre parole è possibile definire numero primo, o semplicemente primo, un numero naturale maggiore di 1 divisibile per 1 e per se stesso.*/
@@ -31,29 +33,29 @@ function nprimi($n){
 function primo($number){
 
   $counter = 0; 
-            for($j=1;$j<=$i;$j++){ 
-  
-  
-                  if($number % $j==0){ 
-  
-                        $counter++;
-                  }
-            }
-  
-          if($counter==2){
-            return true;
-          }else{
-            return false;
-          }
+  for($j=1;$j<=$number;$j++){
+    if($number % $j==0){ 
+
+          $counter++;
+    }
+  }
+
+  if($counter==2){
+    echo "primo";
+    return true;  
+  }else{
+    echo "non primo";
+    return false;
+  }
 }
 
 primo(40);
 
-//es 3 sbagliato
+//----------------------------------------------------------------------------------------------------------------
 
-<?php
-// Your code here!
- 
+//es 3 tombola
+
+//first solution 
 function Tombola(){
   $array = [];
 
@@ -71,4 +73,43 @@ function Tombola(){
 
 Tombola();
 
-?>
+
+//second solution without php functions
+function Tombola1(){
+for ($i=0; $i<=5; $i++) {
+  $num[$i] = rand(1,10);
+  for ($j=0; $j<$i; $j++) {             
+     while ($num[$j] == $num[$i]){               
+        $num[$i] = rand(1,10);               
+        $j = 0;             
+     }           
+  }    
+}    
+sort($num);    
+print_r($num); 
+}
+
+Tombola1();
+
+
+// function to check if an element is in one array
+// function compare($array, $value)
+// {
+//     foreach ($array as $value2) {
+//         if ($value2 == $value) {
+//             return 'true';
+//         } else {
+//             return 'false';
+//         }
+//     }
+// }
+
+
+
+
+
+     
+
+
+
+
